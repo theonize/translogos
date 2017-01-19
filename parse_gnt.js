d76134db6046082c2b036004a842668c512e1770
fingerskier <fingerskier@gmail.com>
2017-01-19 16:00:38 -0600

    GNT parsed; beginning BHS

--- parse_gnt.js
+++ parse_gnt.js
@@ -1,59 +1,40 @@
 const fs = require('fs')
 let data = fs.readFileSync('GNT.NA27.txt')
 let result = {}
+let stuff = []
+let text = ''
+let chapter = 0
+let book = ''
 
 data = data.toString().split('\n')
 
 
 for (let line of data){
-	let stuff
-	let text
-	let verse = 0
-	let volume = 0
-	let chapter = 0
-	let book = ''
+	stuff = line.split(/[:]/)
 
-	stuff = line.split(':')	// split off the verse number
+	if (stuff.length < 2) continue	// ignore header lines
 
-	if (stuff.length < 2) continue
+	book = stuff[0]
 
-	text = stuff[1].split(/\d/)
-
-	stuff = stuff[0].split('|')
-
-	if (stuff.length > 1) {
-		chapter = stuff[1]
-
-		stuff = stuff[0].split(' ')
-
-		book = stuff[0]
- 		volume = stuff[1]
-	} else {
-		stuff = stuff[0].split(' ')
-
-		book = stuff[0]
-		volume = 0
-		chapter = stuff[1]
-	}
-
-	if (!result[book]) result[book] = []
-
-	if (!result[book][chapter]) result[book][chapter] = []
-
-	if (volume) {
-		if (!result[book][chapter][volume])
-			result[book][chapter][volume] = []
-		
-		if (!result[book][chapter][volume][chapter])
-			result[book][chapter][volume][chapter] = []
-
-		result[book][chapter][volume][chapter][verse] = text
-	} else{
-		if (!result[book][chapter][chapter])
-			result[book][chapter][chapter] = []
-
-		result[book][chapter][verse] = text
-	}
+	result[book] = []
 }
+
+for (let line of data){
+	stuff = line.split(/[:]/)
+
+	if (stuff.length < 2) continue	// ignore header lines
+
+	book = stuff[0]
+	chapter = stuff[1] - 1
+	text = stuff[2]
+	.split(/\d/)
+	.filter(function(curr){
+		if (curr.length > 1) return true
+		else return false
+	})
+
+	result[book][chapter] = text
+}
+
 
 console.log(JSON.stringify(result))
